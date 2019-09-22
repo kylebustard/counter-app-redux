c8300afca5c6650556613cb481c0a34d32a5d0c0
Kyle Bustard <bustardkyle@gmail.com>
2019-09-21 22:06:11 -0500

    Implement dispatcher actions

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,29 @@
 const initialState = {
-        counter: 0
+	counter: 0
 }
 
 const reducer = (state = initialState, action) => {
-        return state;
-}
+	if (action.type === 'INCREMENT') {
+		return {
+			counter: state.counter + 1
+		};
+	};
+	if (action.type === 'DECREMENT') {
+		return {
+			counter: state.counter - 1
+		};
+	};
+	if (action.type === 'ADD') {
+		return {
+			counter: state.counter + action.value
+		};
+	};
+	if (action.type === 'SUBTRACT') {
+		return {
+			counter: state.counter + action.value
+		};
+	};
+	return state;
+};
 
 export default reducer;
