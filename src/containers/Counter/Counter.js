06aea39be52d2396c0b05b56582de81a5417333d
Kyle Bustard <bustardkyle@gmail.com>
2019-09-22 23:39:55 -0500

    Split reducers into  and and then combine with

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -16,7 +16,7 @@ class Counter extends Component {
 				<CounterControl label="Add 10" clicked={this.props.onAddCounter} />
 				<CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter} />
 				<hr />
-				<button onClick={this.props.onStoreResult}>Store Result</button>
+				<button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
 				<ul>
 					{this.props.storedResults.map(strResult => {
 						return (<li
@@ -33,8 +33,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
 	return {
-		ctr: state.counter,
-		storedResults: state.results
+		ctr: state.ctr.counter,
+		storedResults: state.res.results
 	};
 };
 
@@ -44,7 +44,7 @@ const mapDispatchToProps = dispatch => {
 		onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
 		onAddCounter: () => dispatch({ type: actionTypes.ADD, value: 10 }),
 		onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, value: 15 }),
-		onStoreResult: () => dispatch({ type: actionTypes.STORE_RESULT }),
+		onStoreResult: (result) => dispatch({ type: actionTypes.STORE_RESULT, result: result }),
 		onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, resultElId: id })
 	};
 };
